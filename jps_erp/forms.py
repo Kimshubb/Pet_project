237412b1c070f51c308352953f6296c29de9a038
kimaniui <kimanifrank209@gmail.com>
2024-05-27 02:50:47 -0700

    reverting to ealier

--- jps_erp/forms.py
+++ jps_erp/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm 
-from wtforms import StringField, PasswordField, SubmitField, SelectField, BooleanField, DecimalField, HiddenField, FloatField, FieldList, FormField
+from wtforms import StringField, PasswordField, SubmitField, SelectField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
 from jps_erp.models import User, Student
 from jps_erp import db
@@ -10,9 +10,7 @@ class User_registrationForm(FlaskForm):
     username = StringField('username', validators=[DataRequired(), Length(min=4, max=20)])
     role = SelectField('role', choices=[('admin', 'Admin'), ('teacher', 'Teacher'), ('accounts', 'Accounts')], validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired(), Length(min=4, max=256)])
-    confirm_password = PasswordField('confirm_password', validators=[DataRequired(), EqualTo('password')])
-    school_name = StringField('school_name', validators=[DataRequired()])
-    school_contacts = StringField('school_contacts', validators=[DataRequired()])  
+    confirm_password = PasswordField('confirm_password', validators=[DataRequired(), EqualTo('password')])  
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
@@ -35,37 +33,10 @@ class Student_registrationForm(FlaskForm):
     guardian_name = StringField('guardian_name', validators=[DataRequired()])
     contact_number1 = StringField('contact_number1', validators=[DataRequired()])
     contact_number2 = StringField('contact_number2', validators=[DataRequired()])
-    grade = SelectField('Grade', choices=[('Playgroup', 'Playgroup'), ('PP1', 'PP1'), ('PP2', 'PP2'),
-                                          ('1', 'Grade 1'), ('2', 'Grade 2'), ('3', 'Grade 3'),
-                                          ('4', 'Grade 4'), ('5', 'Grade 5'), ('6', 'Grade 6')], validators=[DataRequired()])
+    grade = StringField('grade', validators=[DataRequired()])
     submit = SubmitField('Add Student')
 
     def validate_phone_number(self, contact_number1):
         student = db.session.scalar(sa.select(Student).where(Student.contact_number1 == contact_number1.data))
         if student is not None:
             raise ValidationError("Student with this contact number already exists!")
-
-class Fee_paymentForm(FlaskForm):
-    student_name = StringField('student_name', validators=[DataRequired()])
-    student_id = HiddenField('student_id', validators=[DataRequired()])
-    method = SelectField('method', choices=[ 'Cash', 'Mpesa', 'Cheque'], validators=[DataRequired()])
-    amount = DecimalField('amount', validators=[DataRequired()])
-    pay_date = StringField('pay_date', validators=[DataRequired()])
-    code = StringField('code', validators=[DataRequired()])
-    submit = SubmitField('Pay Fees')
-
-#class Additional_feeForm(FlaskForm):
-    #fee_name = StringField('Fee Name', validators=[DataRequired()])
-    #payable_amount = FloatField('Amount', validators=[DataRequired()])
-
-class Fee_structureForm(FlaskForm):
-    grade = SelectField('Grade', choices=[('Playgroup', 'Playgroup'), ('PP1', 'PP1'), ('PP2', 'PP2'),
-                                          ('1', 'Grade 1'), ('2', 'Grade 2'), ('3', 'Grade 3'),
-                                          ('4', 'Grade 4'), ('5', 'Grade 5'), ('6', 'Grade 6')], validators=[DataRequired()])
-    tuition_fee = FloatField('Tuition Fee', validators=[DataRequired()])
-    ass_books = FloatField('Assessment Books', validators=[DataRequired()])
-    diary_fee = FloatField('Diary Fee', validators=[DataRequired()])
-    activity_fee = FloatField('Activity Fee', validators=[DataRequired()])
-    others = FloatField('Other Fees', validators=[DataRequired()])
-    #additional_fees = FieldList(FormField(Additional_feeForm), min_entries=1, max_entries=10)
-    submit = SubmitField('Save')
