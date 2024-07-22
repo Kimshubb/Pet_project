527d924c865178962dd736279e8239620d670561
kimaniui <kimanifrank209@gmail.com>
2024-07-21 22:36:24 -0700

    clone to deploy

--- jps_erp/config.py
+++ jps_erp/config.py
@@ -1,10 +1,12 @@
-import os 
-basedir = os.path.abspath(os.path.dirname(__file__))    # get the directory of the current file
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))  # Get the directory of the current file
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'jps_erp'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
-    'sqlite:///' + os.path.join(basedir, 'jpserp_kisii.db')
-    UPLOAD_FOLDER = '/static/uploads'
+        'mysql://myuser:mypassword@localhost/mydatabase'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # To suppress a warning
+    UPLOAD_FOLDER = os.path.join(basedir, 'static/uploads')
     ALLOWED_EXTENSIONS = {'pdf'}
 
